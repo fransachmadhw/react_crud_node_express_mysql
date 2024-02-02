2e9caefdb0de1a3a6bc8c1c7b507e489f5a85640
frans <franswinata6@gmail.com>
2024-02-02 11:36:14 +0700

    kayak sebelumnya

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,4 @@
 import User from '../models/userModel.js';
-import path from 'path';
-import fs from 'fs';
 
 export const getUsers = async (req, res) => {
   try {
@@ -25,101 +23,21 @@ export const getUserById = async (req, res) => {
 };
 
 export const createUser = async (req, res) => {
-  if (req.files === null)
-    return res.status(400).json({ msg: 'No File Uploaded' });
-
-  const name = req.body.name;
-  const email = req.body.email;
-  const phone = req.body.phone;
-  const file = req.files.file;
-  const fileSize = file.data.length;
-  const ext = path.extname(file.name);
-  const fileName = file.md5 + ext;
-  const url = `${req.protocol}://${req.get(
-    'host'
-  )}/images/${fileName}`;
-  const allowedType = ['.png', '.jpg', '.jpeg'];
-
-  if (!allowedType.includes(ext.toLowerCase()))
-    return res.status(422).json({ msg: 'Invalid Images' });
-  if (fileSize > 5000000)
-    return res
-      .status(422)
-      .json({ msg: 'Image must be less than 5 MB' });
-
-  file.mv(`./public/images/${fileName}`, async (err) => {
-    try {
-      await User.create({
-        name: name,
-        email: email,
-        phone: phone,
-        image: fileName,
-        url: url,
-      });
-      res.status(201).json({ msg: 'User Created Successfuly' });
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).json({ msg: error.message });
-    }
-  });
+  try {
+    await User.create(req.body);
+    res.status(201).json({ msg: 'User Created' });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export const updateUser = async (req, res) => {
-  let fileName = '';
-  const user = await User.findOne({
-    where: {
-      id: req.params.id,
-    },
-  });
-
-  if (!user) return res.status(404).json({ msg: 'No Data Found' });
-
-  if (req.files === null) {
-    fileName = user.image;
-  } else {
-    const file = req.files.file;
-    const fileSize = file.data.length;
-    const ext = path.extname(file.name);
-    fileName = file.md5 + ext;
-    const allowedType = ['.png', '.jpg', '.jpeg'];
-
-    if (!allowedType.includes(ext.toLowerCase()))
-      return res.status(422).json({ msg: 'Invalid Images' });
-    if (fileSize > 5000000)
-      return res
-        .status(422)
-        .json({ msg: 'Image must be less than 5 MB' });
-
-    const filepath = `./public/images/${user.image}`;
-    fs.unlinkSync(filepath);
-
-    file.mv(`./public/images/${fileName}`, (err) => {
-      if (err) return res.status(500).json({ msg: err.message });
-    });
-  }
-
-  const name = req.body.name;
-  const email = req.body.email;
-  const phone = req.body.phone;
-  const url = `${req.protocol}://${req.get(
-    'host'
-  )}/images/${fileName}`;
-
   try {
-    await User.update(
-      {
-        name: name,
-        email: email,
-        phone: phone,
-        image: fileName,
-        url: url,
+    await User.update(req.body, {
+      where: {
+        id: req.params.id,
       },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
-    );
+    });
     res.status(200).json({ msg: 'User Updated' });
   } catch (error) {
     console.log(error.message);
@@ -127,19 +45,7 @@ export const updateUser = async (req, res) => {
 };
 
 export const deleteUser = async (req, res) => {
-  const user = await User.findOne({
-    where: {
-      id: req.params.id,
-    },
-  });
-
-  if (!user) return res.status(404).json({ msg: 'No Data Found' });
-
   try {
-    const filepath = `./public/images/${user.image}`;
-
-    fs.unlinkSync(filepath);
-
     await User.destroy({
       where: {
         id: req.params.id,
